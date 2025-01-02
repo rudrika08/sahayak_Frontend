5592682e1819549b38423d17f9d7eb0d977934f2
Rudrika <rudrika08.panigrahi@gmail.com>
2025-01-02 19:45:24 +0530

    fixes

--- src/authentication/Signup/Signup.jsx
+++ src/authentication/Signup/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "./styles/SignUp.module.scss";
+import styles from "./styles/Signup.module.scss";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
